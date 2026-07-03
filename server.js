e76e6c5530c402ab29826d53868fa2bb697d1817
Soy Gabriel <gsanchezm2@unicartagena.edu.co>
2026-07-03 02:09:12 -0500

    Fase 3: CORS configurado para interfaz web

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const conectarDB = require('./db');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 conectarDB();
@@ -85,7 +87,7 @@ await incidencia.save();
 res.json({ mensaje: "Incidencia reportada exitosamente", incidencia });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 console.log(`CiudadAlerta ejecutándose en puerto ${PORT}`);
 });
